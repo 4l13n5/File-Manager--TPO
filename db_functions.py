9a8407d58e33fbe1d3b989bbf806d7988ebf48a2
thurukan <44681384+thurukan@users.noreply.github.com>
2020-01-10 16:37:28 +0100

    Add files via upload

--- db_functions.py
+++ db_functions.py
@@ -1,19 +1,36 @@
 import sqlite3
+from os import path
 
 # https://www.sqlitetutorial.net/sqlite-python/
 
 insert_command = 'INSERT INTO \"{}\"'\
                 ' VALUES (\"{}\", \"{}\")'
 
-select_command = "SELECT \"{}\" from \"{}\" WHERE tag = \"{}\""
+select_command = "SELECT Datoteka.ID, Datoteka.Path " \
+         "FROM Datoteka " \
+         "JOIN oznacuje on Datoteka.ID=oznacuje.ID " \
+         "JOIN Tag ON oznacuje.Tag = Tag.Tag " \
+         "WHERE Tag.Tag=\'{}\'"
 
 delete_command = "DELETE FROM \"{}\" where tag = \"{}\""
 
+import os
+
+folder_path = os.path.dirname(os.path.realpath(__file__))
+
+create_command = open(folder_path + path.sep + "FileS.sql", "r").read().split("////")
 
 def db_connect(sqlite3_file):
     con = None
     try:
-        con = sqlite3.connect(sqlite3_file)
+        f = False
+        if not path.exists(folder_path + path.sep + "file.db"):
+            f = True
+        con = sqlite3.connect(folder_path + path.sep + sqlite3_file)
+        cur = con.cursor()
+        if f:
+            for str in create_command:
+                cur.execute(str)
         return con
     except sqlite3.Error as err:
         print(err)
@@ -46,9 +63,9 @@ def db_update_linker(con,fileID,tag):
     cur.execute(insert_command.format("oznacuje",fileID, tag))
     con.commit()
 
-def db_select(con, what, table, where):
+def db_select(con, tag):
     cur = con.cursor()
-    cur.execute(select_command.format(what,table,where))
+    cur.execute(select_command.format(tag))
     return cur.fetchall()
 
 
@@ -81,14 +98,49 @@ def extension_index(ext,list=False):
 
 
 #internal use only
-#if __name__ == "__main__":
-#    def db_drop(con):
-#        cur = con.cursor()
-#        cur.execute("DROP TABLE files")
+if __name__ == "__main__":
+    def db_drop():
+        os.remove(folder_path + path.sep + "file.db")
+
+def db_custom(con,command):
+    cur = con.cursor()
+    output = cur.execute(command).fetchall()
+    return output
+
+
+select_command = "SELECT * from Datoteka"
+select2 = "SELECT * from Tag"
+select3 = "SELECT * from oznacuje"
+
+select = "SELECT Datoteka.ID, Datoteka.Path " \
+         "FROM Datoteka " \
+         "JOIN oznacuje on Datoteka.ID=oznacuje.ID " \
+         "JOIN Tag ON oznacuje.Tag = Tag.Tag " \
+         "WHERE oznacuje.Tag=\'{}\'"
 
 
 
+if __name__ == "__main__":
 
+    db_drop()
+    con = db_connect("file.db")
+
+    #Dodamo file in tage
+    db_insert_file(con, "neki/neki/neki.neki")
+    db_insert_tag(con, "Misc", "root")
+    db_insert_file(con, "neki/neki.png")
+    db_insert_tag(con, "Picture", "root")
+
+    #Izpišemo Datoteka tabelo
+    print(db_custom(con, select_command))
+    #izpišemo Tag tabelo
+    print(db_custom(con, select2))
+    #Izpišemo označuje tabelo
+    print(db_custom(con, select3))
+
+    #Selectamo vse
+    print(db_custom(con, select.format("Picture")))
+    print(db_custom(con, select.format("Misc")))
 
 
 
