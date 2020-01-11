933514759a0538e7415d9d86d3a93e2746f47aee
thurukan <44681384+thurukan@users.noreply.github.com>
2020-01-11 11:12:01 +0100

    Add files via upload

--- FileS.sql
+++ FileS.sql
@@ -1,61 +1,51 @@
-/*==============================================================*/
-/* DBMS name:      PostgreSQL 8                                 */
-/* Created on:     09/01/2020 12:21:42                          */
-/*==============================================================*/
 
-/*==============================================================*/
-/* Table: Datoteka                                              */
-/*==============================================================*/
 create table Datoteka (
-   ID                   INT      		not null,
-   Path                 VARCHAR(256)    not null,
-   Name					VARCHAR(256)	not null,
+   ID                   INT      not null,
+   Path                 VARCHAR(256)         null,
    constraint PK_DATOTEKA primary key (ID)
 );
 
-
-/*==============================================================*/
-/* Table: Tag                                                   */
-/*==============================================================*/
+////
 create table Tag (
-   Name                 VARCHAR(64)			 not null,
+   Tag                  VARCHAR(64)			 not null,
    Parent               VARCHAR(64)          null,
-   FOREIGN KEY(Parent) 	REFERENCES Tag(Name),
-   constraint PK_TAG primary key (Name)
+   FOREIGN KEY(Parent) 	REFERENCES Tag(Tag),
+   constraint PK_TAG primary key (Tag)
+);
+////
+create table oznacuje (
+   ID                   INT4                 null,
+   Tag                  VARCHAR(64)          null,
+   FOREIGN KEY(ID) 		REFERENCES Datoteka(ID),
+   FOREIGN KEY(Tag) 	REFERENCES Tag(Tag)
+
+   
+);
+////
+
+create  index OZNACUJE_FK on oznacuje (
+ID
 );
 
-/*==============================================================*/
-/* Table: Oznacuje                                              */
-/*==============================================================*/
-create table Oznacuje (
-   fileID               INT4                 not null,
-   TagName              VARCHAR(64)          not null,
-   FOREIGN KEY(fileID) 		REFERENCES Datoteka(ID),
-   FOREIGN KEY(TagName) 	REFERENCES Tag(Name),
-   constraint PK_OZNACUJE primary key (fileID, TagName)
-);
-
-/*==============================================================*/
-/* Index: OZNACUJE_FK                                           */
-/*==============================================================*/
-create  index OZNACUJE_FK on Oznacuje (
-fileID
-);
-
-/*==============================================================*/
-/* Trigger: PosodobiStarse                                      */
-/*==============================================================*/
+////
 CREATE TRIGGER PosodobiStarse
 BEFORE DELETE
 ON Tag
 BEGIN
 	UPDATE Tag
 	SET Parent = OLD.Parent
-	WHERE Parent = OLD.Name;
-	
+	WHERE Parent = OLD.Tag;
+
 	UPDATE Oznacuje
-	SET TagName = OLD.Parent
-	WHERE TagName = OLD.Name;
+	SET Tag = OLD.Parent
+	WHERE Tag = OLD.Tag;
 END;
 
-
+////
+CREATE TRIGGER izbrisiOznacuje
+AFTER DELETE
+ON Datoteka
+BEGIN
+    DELETE FROM oznacuje
+    WHERE ID = OLD.ID;
+END;
